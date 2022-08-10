ca81734478f7562b97ea786dd71b6dc7ef441ee4
nishuabrol <82961894+nishuabrol@users.noreply.github.com>
2022-08-10 11:56:19 +0800

    is_leap update
    Changed to correct bool values

--- test_leap.c
+++ test_leap.c
@@ -9,11 +9,11 @@
 int is_leap(long year) {
 
   if ((year % 4 == 0 && year % 100 != 0) || (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) ) {
-    return 0;
+    return 1;
   }
 
   else {
-    return 1;
+    return 0;
   }
 
   return 0;
